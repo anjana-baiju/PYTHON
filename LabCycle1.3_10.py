b035fd3933b27c43f8ed18722ec60e2c51a9edbb
Anjana Baiju <12mp1557@rit.ac.in>
2021-02-12 23:23:27 +0530

    deleting

--- LabCycle1.3_10.py
+++
@@ -1,6 +0,0 @@
-data={"Zebra":100,"Bat":200,"Lion":35}
-data1={"Parrot":10,"Sparrow":123,"Chicken":40}
-print("Dict1= ",data)
-print("Dict2= ",data1)
-data.update(data1)
-print("Merged Dictionary : ", data)
--- LabCycle1.3_8.py
+++
@@ -1,7 +0,0 @@
-s1="Hai"
-s2="Popy"
-
-s1_new=s2[0]+s1[1:]
-s2_new=s1[0]+s2[1:]
-
-print(s1_new," ",s2_new)
