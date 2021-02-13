d54d4be19f4c939b024dd59c407d2a736568998c
Anjana Baiju <12mp1557@rit.ac.in>
2021-02-13 11:20:33 +0530

    Added Programs of LAb Cycle 2.2

--- LabCycle1.2_3.py
+++
@@ -1,7 +0,0 @@
-names=["Annie","cucia","dhanya"]
-count=0
-for name in names:
-    for letter in name:
-        if letter =='a' or letter=='A':
-            count=count+1
-print("There are ",count," A in the list")
